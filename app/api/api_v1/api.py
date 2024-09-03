38e93b9fe2787d8e7b8d48ab767f96307731695b
Dennis Mattews <alttert@mail.ru>
2024-09-03 15:20:30 +0300

    flask -> fastapi

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -2,8 +2,10 @@
 # app/api/api_v1/api.py
 
 from fastapi import APIRouter
+
 from app.api.api_v1.endpoints import clicker, products
 
 api_router = APIRouter()
+
 api_router.include_router(clicker.router, prefix="/clicker", tags=["clicker"])
 api_router.include_router(products.router, prefix="/products", tags=["products"])
--- app/main.py
+++ app/main.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.middleware.csrf import CSRFMiddleware
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
 
 from app.api.api_v1.api import api_router
+from app.api.api_v1.endpoints.clicker import router as clicker_router
+from app.api.api_v1.endpoints.products import router as products_router
 from app.core.config import settings
 
 app = FastAPI(
@@ -17,12 +21,14 @@ templates = Jinja2Templates(directory="app/templates")
 
 # Подключение маршрутов
 app.include_router(api_router, prefix=settings.API_V1_STR)
+app.include_router(clicker_router, prefix="/clicker")
+app.include_router(products_router, prefix="/products")
 
 # Подключение CSRF Middleware
 app.add_middleware(CSRFMiddleware, secret=settings.CSRF_SECRET)
 
 
 # Маршрут для главной страницы
-@app.get("/")
-async def root():
-    return {"message": "Welcome to FastHTMX!"}
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
