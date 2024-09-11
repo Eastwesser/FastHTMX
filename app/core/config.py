0f90a988997cb8e6c46d866809e56236955426d7
Dennis Mattews <alttert@mail.ru>
2024-09-11 15:58:54 +0300

    config upd

--- app/core/config.py
+++ app/core/config.py
@@ -42,13 +42,9 @@ class DatabaseConfig(BaseModel):
 
 
 class Settings(BaseSettings):
-    # PROJECT_NAME: str = "FastHTMX Project"
-    # API_V1_STR: str = "/api/v1"
-    # CSRF_SECRET: str = "8148b7148634eeb37192a3d9ebcac7f877a8db21763f667ddaae3d065ba41ce0"
-
-    PROJECT_NAME: str
-    API_V1_STR: str
-    CSRF_SECRET: str
+    PROJECT_NAME: str = "FastHTMX Project"
+    API_V1_STR: str = "/api/v1"
+    CSRF_SECRET: str = "8148b7148634eeb37192a3d9ebcac7f877a8db21763f667ddaae3d065ba41ce0"
 
     model_config = SettingsConfigDict(
         env_file=(".env.template", ".env"),
